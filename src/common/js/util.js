74b6d70a9268d97abb7efbefa6d9d222bd7a100b
李的MBA <lidemba@lideMBAdeMacBook-Air.local>
2017-04-12 11:55:53 +0800

    order-vue2

---
+++ src/common/js/util.js
@@ -0,0 +1,6 @@
+// 解析url参数
+export function urlParse() {
+  let url = window.location.search;
+  let obj = {};
+  let reg =
+}
---
+++ src/main.js
@@ -0,0 +1,39 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from 'components/goods/goods.vue';
+import ratings from 'components/ratings/ratings.vue';
+import seller from 'components/seller/seller.vue';
+
+import 'common/stylus/index.styl';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+const routes = [{
+  path: '/goods',
+  component: goods
+}, {
+  path: '/ratings',
+  component: ratings
+}, {
+  path: '/seller',
+  component: seller
+}];
+
+const router = new VueRouter({
+  linkActiveClass: 'active',
+  routes
+});
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+});
+
+// router.push('goods');
